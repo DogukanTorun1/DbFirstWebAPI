c54b87fe7c353221592c18de171150d59f1f89ec
DogukanTorun1 <ddogukan97@gmail.com>
2024-06-02 17:16:21 +0300

    sevice added & product controller added

---
+++ DbFirstWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using DbFirstWebAPI.Data;
+using DbFirstWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DbFirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly NorthwndContext _db;
+
+        public ProductController(NorthwndContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            if (ModelState.IsValid)
+            {
+
+                List<Product> products = _db.Products.ToList();
+                return Ok(products);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                Product product = _db.Products.Find(id);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                return Ok(product);
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
