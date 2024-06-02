2637ef1fb5345704f6de06bc14ef543dd8ee7783
DogukanTorun1 <ddogukan97@gmail.com>
2024-06-02 16:41:08 +0300

    Database connected and Models created

---
+++ DbFirstWebAPI/Models/CurrentProductList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DbFirstWebAPI.Models;
+
+public partial class CurrentProductList
+{
+    public int ProductId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+}
